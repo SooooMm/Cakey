f642c87b181d5cf7b410638b294f6e7a6d4436a4
SooooMm <soooomm00@gmail.com>
2021-11-08 14:34:47 +0900

    연동 test

--- src/main.java
+++ src/main.java
@@ -1,15 +1,28 @@
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class main extends JFrame{
 	public main() {
 		super("Cakey");
 		setSize(750,1000);
 		setVisible(true);
+		setResizable(false);
 	}
 
 	public static void main(String[] args) {
 		main kiosk = new main();
+		JPanel pn = new JPanel();
+		kiosk.add(pn);
+		GridBagLayout layout = new GridBagLayout();
+		
+		pn.setLayout(layout);
+		
+		JButton start_btn= new JButton();
+		
 		kiosk.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-
 }
