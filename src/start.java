f0fbd3451556bd206022a5567c640a73ac3c7497
yoonjuhee22 <s2014@e-mirim.hs.kr>
2021-12-05 01:27:58 +0900

    수정

--- src/start.java
+++ src/start.java
@@ -40,27 +40,27 @@ public class start extends JFrame{
 		//icon = new ImageIcon("../image/sheet_banila.jpg");//생성자에 ico호출 후, 이미지 경로 지정
 
 		//icon = new ImageIcon("../image/back.jpg");
-		JPanel pn = new JPanel() {
-			Image background = new ImageIcon(Program.class.getResource("../image/back.jpg")).getImage();
+		JPanel pn = new JPanel();
+			/*Image background = new ImageIcon(Program.class.getResource("../image/back.jpg")).getImage();
 			public void paint(Graphics g) { //그리는함수
 				g.drawImage(background,0,0,null); //background를 그려줌
 				setOpaque(false);
 				super.paintComponent(g);
 			}
-		};
+		};*/
 
 		//setSize(1280,720);//JFrame의 크기
-		setResizable(false);//창의 크기를 변경하지 못하게 해준다
+		/*setResizable(false);//창의 크기를 변경하지 못하게 해준다
 		setLocationRelativeTo(null);//창이 가운데 나오게 해준다
 		setLayout(null);
 		setVisible(true);
 		pn.setLayout(null);
 		pn.setBounds(0,0,500,600);
 		add(pn);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//그냥 닫으면 프로그램이 정상적으로 종료
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//그냥 닫으면 프로그램이 정상적으로 종료*/
 
-		pn.setLayout(null);
-
+//		//pn.setLayout(null);
+add(pn);
 		Color s = new Color(204,153,102);
 		JButton start_btn= new JButton("시작"); //시작 버튼
 		start_btn.setText("~주문을 하시려면 여기를 눌러주세요~");
@@ -75,8 +75,8 @@ public class start extends JFrame{
 		manager.setBackground(m);
 		manager.setBounds(640, 20, 100, 50);
 		start_btn.setBounds(0, 815, 750, 175);
-		pn.add(start_btn);
-		pn.add(manager);
+		//pn.add(start_btn);
+		//pn.add(manager);
 
 
 		start_btn.addActionListener(new ActionListener() {
