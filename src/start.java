9e300273ed9316a62917e79492e44ffc44860241
yoonjuhee22 <s2014@e-mirim.hs.kr>
2021-12-06 11:10:28 +0900

    배경 test

--- src/start.java
+++ src/start.java
@@ -27,7 +27,7 @@ import user.UserDAO;
 
 public class start extends JFrame{
 	JScrollPane scrollPane; //컴포넌트에 스크롤 기능을 제공함
-	//ImageIcon icon;
+	ImageIcon icon;
 
 	public start() {
 
@@ -39,15 +39,15 @@ public class start extends JFrame{
 		setResizable(false);
 		//icon = new ImageIcon("../image/sheet_banila.jpg");//생성자에 ico호출 후, 이미지 경로 지정
 
-		//icon = new ImageIcon("../image/back.jpg");
-		JPanel pn = new JPanel();
-			/*Image background = new ImageIcon(Program.class.getResource("../image/back.jpg")).getImage();
+		icon = new ImageIcon("../image/back.jpg");
+		JPanel pn = new JPanel() {
+			//Image background = new ImageIcon(Program.class.getResource("../image/back.jpg")).getImage();
 			public void paint(Graphics g) { //그리는함수
-				g.drawImage(background,0,0,null); //background를 그려줌
+				g.drawImage(icon.getImage(),0,0,null); //background를 그려줌
 				setOpaque(false);
 				super.paintComponent(g);
 			}
-		};*/
+		};
 
 		//setSize(1280,720);//JFrame의 크기
 		/*setResizable(false);//창의 크기를 변경하지 못하게 해준다
@@ -67,18 +67,21 @@ public class start extends JFrame{
 		start_btn.setBackground(s);
 		start_btn.setFont(new FontUIResource("돋움",FontUIResource.BOLD,32));
 		//start_btn.setBackground(ColorUIResource.WHITE);
+		pn.add(start_btn);
+		scrollPane = new JScrollPane(pn);
+		setContentPane(scrollPane);
 		System.out.println("getText: " + start_btn.getText());
-
+         
 		Color m = new Color(211,211,211);
 		JButton manager = new JButton("관리자");
 		//manager.setBackground(ColorUIResource.GRAY);
 		manager.setBackground(m);
 		manager.setBounds(640, 20, 100, 50);
 		start_btn.setBounds(0, 815, 750, 175);
-		pn.add(start_btn);
+		//pn.add(start_btn);
 		pn.add(manager);
-
-
+		scrollPane = new JScrollPane(pn);
+		 setContentPane(scrollPane); 
 		start_btn.addActionListener(new ActionListener() {
 
 			@Override
@@ -105,8 +108,10 @@ public class start extends JFrame{
 
 
 	public static void main(String[] args) {
-		new start();
-
+		start frame = new start();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		   frame.setSize(300, 300);
+	        frame.setVisible(true);
 		new UserDAO();
 
 	}
